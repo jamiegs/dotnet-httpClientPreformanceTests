4bc77907c9b6f9dc9f2738880d0d987c4d6fffe5
Jamie Snell <jgsnell@gmail.com>
2018-10-03 12:39:02 -0500

    write to console rather than debug

--- src/HttpClientPerf/Helpers/PerfTimerLogger.cs
+++ src/HttpClientPerf/Helpers/PerfTimerLogger.cs
@@ -19,10 +19,7 @@ namespace HttpClientPerf
         {
             this._timer.Stop();
             var ms = this._timer.ElapsedMilliseconds;
-
-        // log the performance timing with the Logging library of your choice
-        // Example:
-            System.Diagnostics.Debug.WriteLine(string.Format("{0} - Elapsed Milliseconds: {1}", this._message, ms));
+            Console.WriteLine(string.Format("{0} - Elapsed Milliseconds: {1}", this._message, ms));
         }
     }
 }
